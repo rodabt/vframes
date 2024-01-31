cb468dafac6097a37ba73cb1ba249347d19f1e6e
Rodrigo Abt <rodrigo.abt@gmail.com>
2024-01-30 22:20:56 -0300

    Extended functions

--- main.v
+++ main.v
@@ -17,12 +17,12 @@ struct DContext {
 struct DataFrame {
 	id					string = 'tbl_${rand.ulid()}'
 	ctx					DContext
-mut:
-	display_mode		string = 'box'
-	display_max_rows	int = 100			
+pub:		
 	size				string
-	info				VTable
-	dim					[]int
+	shape				[]int
+pub mut:
+	display_mode		string = 'box'
+	display_max_rows	int = 100
 }
 
 
@@ -44,9 +44,24 @@ fn (ctx DContext) new() DataFrame {
 fn (ctx DContext) read_csv(filename string) DataFrame {
 	id := 'tbl_${rand.ulid()}'
 	mut db := ctx.exec("create table ${id} as select * from '${filename}'")
+	db = ctx.exec("pragma enable_progress_bar")
+	db = ctx.exec("select count(*) as n_rows from ${id}")
+	mut res := db.get_array()
+	num_rows := (res[0]["n_rows"] or {0}).int()
+
+	db = ctx.exec("select count(*) as n_cols from information_schema.columns where table_name = '${id}'")
+	res = db.get_array()
+	num_cols := (res[0]["n_cols"] or {0}).int()
+
+	db = ctx.exec("pragma database_size")
+	res = db.get_array()
+	size := (res[0]["memory_usage"] or {'0'}).str()
+
 	df := DataFrame{
 		id: id
 		ctx: ctx
+		shape: [num_rows, num_cols]
+		size: size
 	}
 	defer { db.close() }
 	return df
@@ -54,19 +69,20 @@ fn (ctx DContext) read_csv(filename string) DataFrame {
 
 fn (df DataFrame) head(n int) string {
 	mut db := df.ctx.exec("select * from ${df.id} limit ${n}")
-	if df.display_mode == 'box' {
-		return db.print_table(max_rows: df.display_max_rows, mode: 'box')
-	} else {
-		return db.get_array()
-	}
+	return db.print_table(max_rows: df.display_max_rows, mode: df.display_mode)
 }
+
+fn (df DataFrame) info() string {
+	mut db := df.ctx.exec("pragma table_info('${df.id}')")
+	return db.print_table(max_rows: df.display_max_rows, mode: df.display_mode)
+}
+
 
 fn main() {
 	mut ctx := DContext{}
-	mut df := ctx.read_csv('tmp/people-100.csv')
+	mut df := ctx.read_csv('tmp/people-500000.csv')
 	println(df.head(5))
-	// println(df.dim)
-    // println(df.describe())
-    // println(df.info())
-    // println(df.shape())
+	println(df.info())
+    println(df.shape)
+    println(df.size)
 }
